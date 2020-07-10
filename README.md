a43d0b04497826cb3b6c444dc3c6ce9bcedcbbba
andreguerra123 <andrecatarinoguerra@protonmail.com>
2020-07-10 12:37:51 +0000

    Java + Gradle

---
+++ README.md
@@ -0,0 +1,15 @@
+# console-java-simple
+
+A simple java "Hello World!" application, buildable with gradle.
+
+
+# Building manually
+
+|        | Task  | Command                    |
+| Gradle | Build | `gradle build`             |
+|        | Run   | `gradle run`               |
+
+
+# App output
+
+App output is streamed into a console. Note that if your app expects user input, do not use command but execute jars in the terminal directly.
---
+++ src/test/java/org/andre/guerra/examples/HelloWorldTest.java
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Red Hat, Inc. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+package org.andre.guerra.examples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Unit test for simple App.
+ */
+@RunWith(JUnit4.class)
+public class HelloWorldTest {
+
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
